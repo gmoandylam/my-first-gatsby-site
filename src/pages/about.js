bb4fef75c84a2b2bcd500253ed8841e7e3bce25c
Andy <a.lam@z.com>
2023-05-16 11:23:39 +0800

    Finished Gatsby Tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,22 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
+import { Link } from 'gatsby'
+import {
+  siteTitle,
+} from '../components/layout.module.css'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p className={siteTitle}>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <Link to="/">
+              Home
+            </Link>
     </Layout>
   )
 }
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="About Me" />
 
 export default AboutPage
